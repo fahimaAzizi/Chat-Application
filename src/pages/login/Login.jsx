c277e8b6dbdd071d6361870613fae36ba784c63b
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-15 22:01:00 +0500

    update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 
 const Login = () => {
+  const [currSatate, setCurrState] = useState("Sign up")
+
+
   return (
     <div className='login'>
 
       <img src={assets.logo_big} alt="" className="logo" />
 
       <form className="login-form">
-        <h2>Sign Up</h2>
+        <h2>{currSatate}</h2>
 
         <input
           type="text"
