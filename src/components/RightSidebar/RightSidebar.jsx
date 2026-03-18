b1349086b08c838d6541e52e2257e6a64f1b0e5c
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-18 21:02:36 +0500

    update

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './RightSidebar.css'
 
 function RightSidebar() {
   return (
--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Lefght'
+import './chat.css'
+
 import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
 function chat() {
   return (
     <div>
       <div>
         <RightSidebar/>
+        <LeftSidebar/>
+        <ChatBox/>
       </div>
     </div>
   )
