745b65353fc4db1e268363434a366d5c69be6254
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-18 21:20:21 +0500

    update

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import assets from '../../assets/assets'
 
 function LeftSidebar() {
   return (
-    <div>LeftSidebar</div>
+    <div className='ls'>
+      <div className='ls-nav'>
+        <img src={assets.logo} className='logo' alt="" />
+        <div>
+          <img src={assets.menu_icon} alt="" />
+        </div>
+      </div>
+      <div>
+        <img src={assets.search_icon} alt="" />
+        <input type='text' placeholder='Search here..'/>
+      </div>
+    </div>
+    
+   
   )
 }
 
