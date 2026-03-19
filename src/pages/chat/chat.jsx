d90ae9973a68257ac191d763d119e67b34315ad1
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-19 20:46:29 +0500

    update

--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Lefght'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 function chat() {
   return (
-    <div>chat</div>
+    <div>
+      <div>
+        <RightSidebar/>
+      </div>
+    </div>
   )
 }
 
