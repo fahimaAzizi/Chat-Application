69a889f8ed910576008b0b4e9cd71d3b7945842a
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-23 16:48:45 +0500

    update

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -10,6 +10,33 @@ const ChatBox = () => {
         <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
         <img src={assets.help_icon} className='help' alt="" />
       </div>
+      <div className="chat-msg">
+
+  <div className="s-msg">
+    <p className="msg">Lorem ipsum is placeholder text commonly used in...</p>
+    <div>
+      <img src={assets.profile_img} alt="" />
+      <p>2:30 PM</p>
+    </div>
+  </div>
+
+
+  <div className="s-msg">
+    <p className="msg">Lorem ipsum is placeholder text commonly used in...</p>
+    <div>
+      <img src={assets.profile_img} alt="" />
+      <p>2:30 PM</p>
+    </div>
+  </div>
+  <div className="r-msg">
+    <p className="msg">Lorem ipsum is placeholder text commonly used in...</p>
+    <div>
+      <img src={assets.profile_img} alt="" />
+      <p>2:30 PM</p>
+    </div>
+  </div>
+
+</div>
 
       <div className="chat-input">
         <input type="text" placeholder='Send a message' />
