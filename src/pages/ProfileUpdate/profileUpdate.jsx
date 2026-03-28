6be5a6db48f3adeb96d306b321c802505a51820a
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-28 21:27:46 +0500

    update

--- src/pages/ProfileUpdate/profileUpdate.jsx
+++ src/pages/ProfileUpdate/profileUpdate.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+  const [image,setImage] = useState(false);
+
   return (
     <div className='profile'>
       <div className="profile-container">
@@ -11,7 +13,7 @@ const ProfileUpdate = () => {
           <h3>Profile Details</h3>
 
           <label htmlFor="avatar">
-            <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <input onChange={(e)=>setImage(e.target.file[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
             <img src={assets.avatar_icon} alt="" />
             upload profile image
           </label>
