b4e10cd29f7c1946992dabb2f8d1efde00b77524
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-29 14:26:59 +0500

    update

--- src/pages/ProfileUpdate/profileUpdate.jsx
+++ src/pages/ProfileUpdate/profileUpdate.jsx
@@ -3,7 +3,8 @@ import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
-  const [image,setImage] = useState(false);
+
+  const [image, setImage] = useState(false);
 
   return (
     <div className='profile'>
@@ -13,18 +14,33 @@ const ProfileUpdate = () => {
           <h3>Profile Details</h3>
 
           <label htmlFor="avatar">
-            <input onChange={(e)=>setImage(e.target.file[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
-            <img src={ image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id='avatar'
+              accept='.png, .jpg, .jpeg'
+              hidden
+            />
+
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
+
             upload profile image
           </label>
 
-          <input type="text" placeholder='Your name' required/>
+          <input type="text" placeholder='Your name' required />
           <textarea placeholder='Write profile bio' required></textarea>
 
           <button type="submit">Save</button>
         </form>
 
-        <img src={assets.logo_icon} alt="" />
+        <img
+          className='profile-pic'
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt=""
+        />
 
       </div>
     </div>
