9146e6d1dd8ac61adb519bb065bcfc63f8e55b9d
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-30 17:57:04 +0500

    update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyC-Li45VMRhmZDkYfumZq-BBQz8OcHTaIY",
   authDomain: "chat-app-630a2.firebaseapp.com",
@@ -12,5 +13,31 @@ const firebaseConfig = {
   measurementId: "G-2SG73FH74M"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+
+    await setDoc(doc(db, "users", user.uid), {
+      id: user.uid,
+      username: username.toLowerCase(),
+      email,
+      name: "",
+      avatar: "",
+      bio: "Hey, There i am using chat app",
+      lastSeen: Date.now(),
+    });
+
+    await setDoc(doc(db, "chats", user.uid), {
+      chatData: []
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
