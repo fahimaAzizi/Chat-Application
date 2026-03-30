5b8ddb85141a0f06633e76765838c3e087e013ec
Fahima Azizi <fahimaazizi397@gmail.com>
2026-03-30 18:49:37 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
+import ProfileUpdate from './pages/ProfileUpdate/profileUpdate'
 import Chat from './pages/Chat/Chat'   // ✅ FIXED
 import Login from './pages/login/Login'
+import { ToastContainer ,toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 const App = () => {
   return (
     <>
+    <ToastContainer/>
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/chat' element={<Chat/>} />
