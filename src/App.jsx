bceb01b9a632f20981635bbd12644f90b9e7cece
Fahima Azizi <fahimaazizi397@gmail.com>
2026-04-02 14:06:29 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import ProfileUpdate from './pages/ProfileUpdate/profileUpdate'
 import Chat from './pages/Chat/Chat'   // ✅ FIXED
 import Login from './pages/login/Login'
 import { ToastContainer ,toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
+import { onAuthStateChanged } from 'firebase/auth'
+import {auth} from './config/firebase'
 
 const App = () => {
+  useEffect(()=>{
+    onAuthStateChanged(auth, async (user)=> {
+      if (user) {
+
+      }
+    })
+  },[])
   return (
     <>
     <ToastContainer/>
