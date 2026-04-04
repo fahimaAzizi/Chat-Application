ae9fd0b5335c04c87e1702b2c23a37ead9551f78
Fahima Azizi <fahimaazizi397@gmail.com>
2026-04-04 22:26:06 +0500

    update

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,21 +1,18 @@
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { resolvePath } from "react-router-dom";
 
 
 const upload = async(file) => {
 
     const storage = getStorage();
-    const storageRef = ref(storage, 'images/rivers.jpg');
+    const storageRef = ref(storage, `images/${Data.now() + file.name}`);
 
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
+
     uploadTask.on('state_changed',
         (snapshot) => {
-            // Observe state change events such as progress, pause, and resume
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            
             const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             console.log('Upload is ' + progress + '% done');
             switch (snapshot.state) {
@@ -28,13 +25,12 @@ const upload = async(file) => {
             }
         },
         (error) => {
-            // Handle unsuccessful uploads
+            
         },
         () => {
-            // Handle successful uploads on complete
-            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                console.log('File available at', downloadURL);
+                resolve(downloadURL)
             });
         }
     );
