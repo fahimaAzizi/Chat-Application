cc2751af0a0a14e46bf18903a084eda3cf41eb4c
Fahima Azizi <fahimaazizi397@gmail.com>
2026-04-09 19:47:45 +0500

    update

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -5,90 +5,38 @@ import './LeftSidebar.css'
 const LeftSidebar = () => {
   return (
     <div className='ls'>
-
-      {/* Top Navbar */}
+     <div className='ls-top'>
       <div className='ls-nav'>
         <img src={assets.logo} className='logo' alt="" />
         <div className="menu">
           <img src={assets.menu_icon} alt="" />
-          <div className='sun-menu'>
+          <div className='sub-menu'>
             <p>Edit profile</p>
             <hr />
             <p>Logout</p>
           </div>
         </div>
       </div>
+     
 
       {/* Search */}
       <div className="ls-search">
         <img src={assets.search_icon} alt="" />
         <input type="text" placeholder='Search here..' />
       </div>
-
+      </div>
       {/* Friends List */}
       <div className="ls-list">
-        <div className="friends">
+      {Array(12).fill("").map((item,index)=>(
+  <div key={index} className="friends">
           <img src={assets.profile_img} alt="" />
           <div>
             <p>Richard Sanford</p>
-           
+           <span>Hello , how are you?</span>
           </div>
         </div>
-      </div>
-       <div className="ls-list">
-        <div className="friends">
-          <img src={assets.profile_img} alt="" />
-          <div>
-            <p>Richard Sanford</p>
-            
-           
-          </div>
-        </div>
-      </div>
-       <div className="ls-list">
-        <div className="friends">
-          <img src={assets.profile_img} alt="" />
-          <div>
-            <p>Richard Sanford</p>
-           
-          </div>
-        </div>
-      </div>
-       <div className="ls-list">
-        <div className="friends">
-          <img src={assets.profile_img} alt="" />
-          <div>
-            <p>Richard Sanford</p>
-           
-          </div>
-        </div>
-      </div>
-       <div className="ls-list">
-        <div className="friends">
-          <img src={assets.profile_img} alt="" />
-          <div>
-            <p>Richard Sanford</p>
-           
-          </div>
-        </div>
-      </div>
-       <div className="ls-list">
-        <div className="friends">
-          <img src={assets.profile_img} alt="" />
-          <div>
-            <p>Richard Sanford</p>
-           
-          </div>
-        </div>
-      </div>
-       <div className="ls-list">
-        <div className="friends">
-          <img src={assets.profile_img} alt="" />
-          <div>
-            <p>Richard Sanford</p>
-           
-          </div>
-        </div>
+      ))}
+      
       </div>
 
     </div>
