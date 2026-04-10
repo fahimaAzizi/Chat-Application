344b350e468ca2877b0cb95dfd271cf2852d4367
Fahima Azizi <fahimaazizi397@gmail.com>
2026-04-10 22:46:00 +0500

    update

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -37,9 +37,40 @@ const AppContextProvider = () => {
       },60000)
 
     } catch (error) {
-      console.log(error);
+      
     }
   };
+  useEffect(() => {
+  if (userData) {
+    const chatRef = doc(db, "chats", userData.id);
+
+    const unSub = onSnapshot(chatRef, async (res) => {
+      const chatItems = res.data().chatsData;
+
+      const tempData = [];
+
+      for (const item of chatItems) {
+        const userRef = doc(db, "users", item.rId);
+        const userSnap = await getDoc(userRef);
+
+        const userData = userSnap.data();
+
+        tempData.push({
+          ...item,
+          userData
+        });
+      }
+
+      setChatData(
+        tempData.sort((a, b) => b.updatedAt - a.updatedAt)
+      );
+    });
+
+    return () => {
+      unSub();
+    };
+  }
+}, [userData]);
   
 
   const value = {
