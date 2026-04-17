ed11e3f34f4a6aefb946ff8ed3eac6e259609816
Fahima Azizi <fahimaazizi397@gmail.com>
2026-04-17 23:16:10 +0500

    update

--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './Chat.css'
 import RightSidebar from '../../components/RightSidebar/RightSidebar'
 import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
 import ChatBox from '../../components/ChatBox/ChatBox'
+import { AppContext } from '../../context/AppContext'
 
 function Chat() {
+  const {chatData,userData} = useContext(AppContext)
+  const [loading,setLoading] = useState(true)
   return (
     <div className='chat'>
-      <div className='chat-container'>
-        
+      {
+        loading
+        ?<p className='loading'>Loading...</p>
+        :
+        <div className='chat-container'>
         <LeftSidebar />
         <ChatBox />
         <RightSidebar />
+        </div>
+      }
+      
+        
+        
 
-      </div>
+      
     </div>
   )
 }
