b227690a84ad2ed345ee595621b64ad0d542b199
Fahima Azizi <fahimaazizi397@gmail.com>
2026-04-19 19:34:26 +0500

    update

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -4,89 +4,83 @@ import assets from '../../assets/assets'
 import { AppContext } from '../../context/AppContext'
 import { arrayUnion, doc, onSnapshot, updateDoc } from 'firebase/firestore'
 import { db } from '../../config/firebase'
-import { array } from 'firebase/firestore/pipelines'
 
 const ChatBox = () => {
-  const {userData,messagesId,chatUser,messages,setMessages} = useContext(AppContext);
-  const [input,setInput] =useState("");
-
-
+  const { userData, messagesId, chatUser, messages, setMessages } = useContext(AppContext);
+  const [input, setInput] = useState("");
 
   const sendMessage = async () => {
     try {
       if (input && messagesId) {
-        await updateDoc(doc(db,'messages',messagesId),{
-          messages:arrayUnion({
-            sId:userData.id,
-            text:input,
-            createdAt:new Data()
+        await updateDoc(doc(db, 'messages', messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            text: input,
+            createdAt: new Date()
           })
-        })
+        });
+        setInput(""); // clear input
       }
     } catch (error) {
-      
+      console.error(error);
     }
-  }
-   
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     if (messagesId) {
-      const unsub = onSnapshot(doc(db, 'messages', messagesId),(res)=>{
-        setMessages(res.data().messages.revers())
-        console.log(res.data().messages.revers());
-      })
-      return ()=>{
-        unsub();
-      }
+      const unsub = onSnapshot(doc(db, 'messages', messagesId), (res) => {
+        const msgs = res.data()?.messages || [];
+        setMessages(msgs.reverse());
+      });
+
+      return () => unsub();
     }
-  },[messagesId])
+  }, [messagesId]);
 
-
-
-  return (
+  return messagesId ? (
     <div className='chat-box'>
+
+      {/* Header */}
       <div className="chat-user">
-        <img src={chatUser.userData.avatar} alt="" />
-        <p>{chatUser.userData.name}<img className='dot' src={assets.green_dot} alt="" /></p>
+        <img src={chatUser?.userData?.avatar || assets.profile_img} alt="" />
+        <p>
+          {chatUser?.userData?.name}
+          <img className='dot' src={assets.green_dot} alt="" />
+        </p>
         <img src={assets.help_icon} className='help' alt="" />
       </div>
+
+      {/* Messages */}
       <div className="chat-msg">
+        {messages?.map((msg, index) => (
+          <div key={index} className={msg.sId === userData.id ? "s-msg" : "r-msg"}>
+            <p className="msg">{msg.text}</p>
+          </div>
+        ))}
+      </div>
 
-  <div className="s-msg">
-    <p className="msg">Lorem ipsum is placeholder text commonly used in...</p>
-    <div className=''>
-      <img src={assets.profile_img} alt="" />
-      <p>2:30 PM</p>
-    </div>
-  </div>
-
-  <div className="s-msg">
-   <img className='msg-img' src={assets.profile_img} alt="" />
-    <div>
-      <img src={assets.profile_img} alt="" />
-      <p>2:30 PM</p>
-    </div>
-  </div>
-
-  <div className="r-msg">
-    <p className="msg">Lorem ipsum is placeholder text commonly used in...</p>
-    <div>
-      <img src={assets.profile_img} alt="" />
-      <p>2:30 PM</p>
-    </div>
-  </div>
-
-</div>
-
+      {/* Input */}
       <div className="chat-input">
-        <input type="text" placeholder='Send a message' />
-        <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
+        <input 
+          onChange={(e) => setInput(e.target.value)} 
+          value={input} 
+          type="text" 
+          placeholder='Send a message' 
+        />
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
         </label>
-        <img src={assets.send_button} alt="" />
+        <img src={assets.send_button} alt="" onClick={sendMessage} />
       </div>
+
     </div>
-  )
+  ) : (
+    <div className='chat-welcome'>
+      <img src={assets.logo_icon} alt="" />
+      <p>chat anytime, anywhere</p>
+    </div>
+  );
 }
 
-export default ChatBox
+export default ChatBox;
